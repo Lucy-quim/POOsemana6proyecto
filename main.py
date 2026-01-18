771a3f273b9baeecac4226359a8b8bef6ef0284c
Lucy-quim <churuchumbilucy@gmail.com>
2026-01-18 18:07:18 -0500

    Nuevos cambios

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+#importamos datos de carpeta y archivo modelo y servicio
 from Modelos.modelo import Usuario
 from Servicios.servicio import validar_mayoria_edad
 
 if __name__ == "__main__":
-    nuevo_usuario = Usuario("Jaime", 20, "jaime@gmail.com")
+    #datos de usuario
+    nuevo_usuario = Usuario("Jaime", 20,"jaime@gmail.com")
     es_mayor = validar_mayoria_edad(nuevo_usuario)
-    print(f"{nuevo_usuario.nombre} es mayor de edad: {es_mayor}")
+    #correo = Usuario("jaime@gmail.com")
+    #imprime datos 
+    print(f"{nuevo_usuario.nombre} es mayor de edad: {es_mayor} correo:{nuevo_usuario.correo}")
